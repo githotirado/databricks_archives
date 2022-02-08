d93d76acb10be7853e5fbe15c7347d7a3057b218
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2022-02-08 09:49:06 -0800

    additional methods for restaurant

--- class/restaurants.py
+++ class/restaurants.py
@@ -12,6 +12,7 @@ class Restaurant:
         """Makes restaurant objects with name and cuisine type"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
     
     def describe_restaurant(self):
         """Sets forth a restaurant description"""
@@ -21,13 +22,30 @@ class Restaurant:
         """Method to open a restaurant that might be closed"""
         print(f"The restaurant {self.restaurant_name} is open.")
 
+    def set_number_served(self, ns):
+        """Hard set of number served"""
+        self.number_served = ns
+
+    def increment_number_served(self, inc_number):
+        """Dynamically increment number served"""
+        self.number_served += inc_number
+
 restaurant1 = Restaurant("La Boheme", "French")
 restaurant2 = Restaurant("Mario's", "Peruvian")
 restaurant3 = Restaurant("El Morfi", "Argentine")
 
-print(f"Manual invocation: {restaurant1.restaurant_name} serves {restaurant1.cuisine_type} food")
 restaurant1.describe_restaurant()
-restaurant1.open_restaurant()
+print(f"Number served: {restaurant1.number_served}")
+restaurant1.number_served = 72
+print(f"New number served: {restaurant1.number_served}")
+restaurant1.set_number_served(54)
+print(f"Newest number served is {restaurant1.number_served}")
+my_inc = 12
+restaurant1.increment_number_served(my_inc)
+print(f"Incremented number by {my_inc} resulting in {restaurant1.number_served}")
+# print(f"Manual invocation: {restaurant1.restaurant_name} serves {restaurant1.cuisine_type} food")
+# restaurant1.describe_restaurant()
+# restaurant1.open_restaurant()
 
-restaurant2.describe_restaurant()
-restaurant3.describe_restaurant()
+# restaurant2.describe_restaurant()
+# restaurant3.describe_restaurant()
