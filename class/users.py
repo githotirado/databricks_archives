22dffe81efa8269c0ed3f984e116a858abeb358a
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2022-02-08 16:01:49 -0800

    admin rights to class user

--- class/users.py
+++ class/users.py
@@ -31,19 +31,37 @@ class User:
         """reset the login attempts back to zero"""
         self.login_attempts = 0
 
-henry = User("henry", "tirado", "analytics", "Jones", "5.10")
-kirk = User("kirk", "wagner", "entertainment", "Plisken", "6.1")
-sean = User("sean", "mcVay", "sports", "rampage", "5.11")
+class Admin(User):
+    """Special kind of user is the admin"""
+    def __init__(self, first_name, last_name, department, manager, height):
+        """Attributes of the admin user"""
+        super().__init__(first_name, last_name, department, manager, height)
+        self.privileges = ["can add post", "can delete post", "can ban user", "can read post"]
 
-henry.describe_user()
-print(f"{henry.greet_user()}")
-henry.increment_login_attempts()
-henry.increment_login_attempts()
-henry.increment_login_attempts()
-henry.increment_login_attempts()
-henry.describe_user()
-henry.reset_login_attempts()
-henry.describe_user()
+    def show_privileges(self):
+        print(f"Privileges for this user: {self.privileges}")
+
+# Exercise 9-7 Admin
+ht_admin = Admin("henry", "tirado", "analytics", "Jones", "5.10")
+ht_admin.describe_user()
+ht_admin.show_privileges()
+
+# henry = User("henry", "tirado", "analytics", "Jones", "5.10")
+# kirk = User("kirk", "wagner", "entertainment", "Plisken", "6.1")
+# sean = User("sean", "mcVay", "sports", "rampage", "5.11")
+
+# Exercise 9-5
+# henry.describe_user()
+# print(f"{henry.greet_user()}")
+# henry.increment_login_attempts()
+# henry.increment_login_attempts()
+# henry.increment_login_attempts()
+# henry.increment_login_attempts()
+# henry.describe_user()
+# henry.reset_login_attempts()
+# henry.describe_user()
+
+# Exercise 9-3
 # kirk.describe_user()
 # print(f"{kirk.greet_user()}")
 # sean.describe_user()
