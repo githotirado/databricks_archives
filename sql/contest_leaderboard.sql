0754e5a3812398fc5ef2d49608e58886608ed4b8
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2022-02-11 22:52:50 -0800

    SQL for weather, contest, placements

--- sql/contest_leaderboard.sql
+++ sql/contest_leaderboard.sql
@@ -1,5 +1,11 @@
-select hacker_id, sum(max_score)
+
+-- Contest Leaderboard
+select s.hacker_id, h.name, sum(s.max_score) ssum
 from
-(select hacker_id, challenge_id, max(score) max_score
-from submissions
-group by hacker_id, challenge_id)
+    (select hacker_id, challenge_id, max(score) max_score
+    from submissions
+    group by hacker_id, challenge_id) as s
+left outer join hackers h on s.hacker_id = h.hacker_id
+group by s.hacker_id, h.name
+having ssum > 0
+order by ssum desc, s.hacker_id asc
