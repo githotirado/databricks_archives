4deddf1b7966686edd29fc1aa3afdd1febe306a9
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2022-03-11 22:07:01 -0800

    alternate Project Planning SQL select

--- sql/sql_project_planning.sql
+++ sql/sql_project_planning.sql
@@ -1,24 +1,28 @@
--- select p1.task_id, 
---     p1.start_date p1s, 
---     p1.end_date p1e,
---     p2.task_id,
---     p2.start_date p2s,
---     case
---     when p1.end_date = p2.start_date then "same"
---     else "new"
---     end as sequence
--- from projects p1
--- join projects p2 on p1.end_date=p2.start_date
-
+-- Solution first time (MySQL) [has different datediff implementation]
 select start_date, min(end_date)
+    -- , datediff(min(end_date), start_date)
 from
     (select start_date 
     from projects
-    where start_date not in (select end_date from projects)) a,
-
+    where start_date not in (select end_date from projects)) a,  
     (select end_date
     from projects
     where end_date not in (select start_date from projects)) b
 where start_date < end_date
 group by start_date
-order by (datediff(start_date, min(end_date))) desc, start_date asc;
+order by datediff(min(end_date), start_date) asc, start_date asc;
+
+-- Solution second time (MSSQL) [has alternate datediff implementation]
+select sd.start_date
+        , min(ed.end_date)
+       -- , datediff(day, sd.start_date, min(ed.end_date))
+from
+    (select start_date
+    from projects
+    where start_date not in (select end_date from projects)) as sd,
+    (select end_date
+    from projects
+    where end_date not in (select start_date from projects)) as ed
+where sd.start_date < ed.end_date
+group by sd.start_date
+order by datediff(day, sd.start_date, min(ed.end_date)) asc, sd.start_date asc;
