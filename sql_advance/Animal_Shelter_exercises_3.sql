97cd2dbe0eb7f3da2db2fb8980a9c54f71edaa30
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2022-04-17 14:19:29 -0700

    AS - changed subquery to window rank function

--- sql_advance/Animal_Shelter_exercises_3.sql
+++ sql_advance/Animal_Shelter_exercises_3.sql
@@ -39,6 +39,8 @@ where admission_date > '2017-08-01' and species = 'Dog';
 /* 
 Window functions / partitioning and framing
 */
+
+-- select species, name, checkup_time, heart_rate, species_avg, all with heart rate above species avg
 with checkup as (select species
 	, name
 	, checkup_time
@@ -46,7 +48,7 @@ with checkup as (select species
 	, cast(avg(heart_rate) over (partition by species) as decimal(5, 2)) as species_avg
 	, min(heart_rate) over (partition by species, name) as animal_min
 from routine_checkups)
--- select species, name, checkup_time, heart_rate, species_avg,
+
 select distinct species, name, heart_rate, species_avg,
  		every (heart_rate >= species_avg) over (partition by species, name) as bool_value
 --  		case
@@ -57,7 +59,8 @@ from checkup
 where animal_min >= species_avg
 order by species asc, name asc --, checkup_time asc;
 
-/* 
+/*
+=================================================================================
 get vaccine counts per year + avg of previous 2 years per line and percentage.
 */
 with vacc_data as (select date_part('year', vaccination_time) as year
@@ -72,3 +75,28 @@ vacc_data_2 as (select *
 select *
 	, cast((number_of_vaccinations * 100 / previous_2_yr_avg) as decimal(5,2)) as percent_change
 from vacc_data_2;
+
+/*
+extract(year from vaccination_time)
+  is same as
+date_part('year', vaccination_time)
+=================================================================================
+*/
+
+/* postgreSQL
+Show year, month, monthly adoption total, percent of annual total
+*/
+with adopt_table as (
+						select date_part('year', adoption_date) as year
+							, date_part('month', adoption_date) as month
+							, sum(adoption_fee) as monthly_revenue
+						from adoptions
+						group by date_part('year', adoption_date), date_part('month', adoption_date)
+					)
+select year
+		, month
+		, monthly_revenue
+		, sum(monthly_revenue) over (partition by year) as yearly
+		, cast(monthly_revenue * 100 / (sum(monthly_revenue) over (partition by year)) as decimal(5,2)) as pct
+from adopt_table
+order by year, month;
